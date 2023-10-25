a53fe479e5105a0231dcc03f078a731c84374c51
Gaful <114806464+GafulOne@users.noreply.github.com>
2023-10-25 22:29:41 +0300

    Add files via upload

---
+++ Avto/Class1.cs
@@ -0,0 +1,60 @@
+﻿public class Avto
+{
+    private string Number; // Номер машины
+    private int TopFuel; // Вместимость бака автомобиля
+    private double Fuel; // Количество топлива в баке
+    private double Ras; // Расход топлива на 100км
+    private double Distance; // Растояние
+    private double Milleage; //Пробег
+
+
+    public Avto(string Number, int TopFuel, double Fuel, double Ras) //Конструктор класса
+    {
+        this.Number = Number;
+        this.Fuel = Fuel;
+        this.Ras = Ras;
+        this.TopFuel = TopFuel;
+        this.Milleage = 0;
+    }
+
+    public void Out() //Метод выводы информации о машне
+    {
+        Console.WriteLine($"Номер машины: {Number} \nВместимость бака: {TopFuel} \nКоличество топлива в баке: {Fuel} \nРасход топлива на 100км: {Ras}");
+    }
+
+    public void Refuel() // Метод заправки автомобиля
+    {
+        Console.WriteLine($"На сколько литров вы хотите заправиться? Максимально количество {TopFuel}. Топлива в баке: {Fuel}");
+        double Amount = Convert.ToDouble(Console.ReadLine());
+        if (Fuel + Amount <= TopFuel) 
+        {
+            Fuel += Amount;
+            Console.WriteLine($"Вы заправились на {Amount} литров. Текущее количество топлива {Fuel}");
+        }
+        else
+        {
+            Console.WriteLine("Бак переполнен");
+        }
+    }
+
+    public void Drive(double Distance) //Метод езды автомобиля
+    {
+
+        while (Milleage != Distance) //цикл пока пробег не равен заданной дистанции
+        {
+            Fuel -= Ras;
+            Milleage += 100;
+            Console.WriteLine($"Пройдено {Milleage}. Остаток топлива в баке: {Fuel}");
+            if (Fuel < 0)
+            {
+                Console.WriteLine("У вас кончилось топливо");
+                return;
+            }
+            if (Fuel < Ras)
+            {
+                Console.WriteLine($"Недостаточно топлива чтобы проехать {Distance} км. Заправьте машину");
+                this.Refuel();
+            }
+        }
+    }
+}
---
+++ Avto/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassDZ
+{
+    class Program
+    {
+        static void Main()
+        {
+            Avto car = new Avto("AV123C", 50, 20, 7); // Номер машины, Вместимость бака автомобиля, Количество топлива в баке, Расход топлива на 10км
+
+            car.Out(); //Вывод информации о машине
+            Console.WriteLine("Укажите расстояние для поездки:");
+            double distance = Convert.ToDouble(Console.ReadLine());
+
+            car.Drive(distance);
+        }
+    }
+}
